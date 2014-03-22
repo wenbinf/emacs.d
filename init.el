e0a8deeac62fef44aa16106d2ce29bf7c3d0a81f
Wenbin Fang <wenbin@wenbin.org>
2014-03-22 18:58:52 +0000

    Add go-mode

--- init.el
+++ init.el
@@ -124,3 +124,14 @@
 (package-initialize)
 (unless (package-installed-p 'scala-mode2)
   (package-refresh-contents) (package-install 'scala-mode2))
+
+(package-initialize)
+(unless (package-installed-p 'go-mode)
+  (package-refresh-contents) (package-install 'go-mode))
+
+(require 'go-mode-load)
+(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c i") 'go-goto-imports)))
